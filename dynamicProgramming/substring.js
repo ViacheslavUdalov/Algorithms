40ee71a8cc5822a28c4ad53a72e720e8b7ab6010
Vyacheslav Udalov <slava187115@gmail.com>
2024-11-23 09:49:02 +0300

    Revert "longest common substring, greatest increases substring, levinstein."
    This reverts commit 9719661bae69b59be4b59dce989164fb3e7bf665.

--- dynamicProgramming/substring.js
+++
@@ -1,9 +0,0 @@
-function substring(a) {
-    if (a.length <= 1) {
-        return 0;
-    }
-    let arrayOfString = a.split('');
-    let arrayOfNumbers = Array.from({length: arrayOfString.length}).fill(0);
-
-    
-}
