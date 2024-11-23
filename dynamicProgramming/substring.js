18182282104697e6deffec65e9f65ce9e9303de5
Vyacheslav Udalov <slava187115@gmail.com>
2024-11-23 09:54:23 +0300

    longest common substring, greatest increasing substring, levinstein.

---
+++ dynamicProgramming/substring.js
@@ -0,0 +1,9 @@
+function substring(a) {
+    if (a.length <= 1) {
+        return 0;
+    }
+    let arrayOfString = a.split('');
+    let arrayOfNumbers = Array.from({length: arrayOfString.length}).fill(0);
+
+    
+}
